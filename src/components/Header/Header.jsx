3e50b93d09843a0d284e6aadd65850cfaeae1181
dewansh01 <dewansh.mca21.du@gmail.com>
2025-04-01 06:17:41 +0530

    Made header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types"
+import styles from "./Header.module.css"
+const Header = ({title}) => {
+    return(
+        <Header className={styles.header}>
+            <h1>{title}</h1>
+            <nav>
+                <ul className={styles.navList}>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+            </nav>
+        </Header>
+    )
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+};
+
+Header.defaultProps = {
+    title: "My Website",
+};
+
+export default Header;
