f143b15178eee35a54598c6976695f5288f6720b
dewanshpratapsingh <dewansh.mca21.du@gmail.com>
2025-04-11 10:20:57 +0000

    using Link inplace of a tag

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types"
+import Link from "react"
 import styles from "./Header.module.css"
 const Header = ({title}) => {
     return(
@@ -7,9 +8,9 @@ const Header = ({title}) => {
             <h1>{title}</h1>
             <nav>
                 <ul className={styles.navList}>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/about">About</Link></li>
+                    <li><Link href="/contact">Contact</Link></li>
                 </ul>
             </nav>
         </header>
