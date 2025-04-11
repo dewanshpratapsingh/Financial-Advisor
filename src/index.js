b57e6067c56390627cacd2358afed41161c60ab7
dewanshpratapsingh <dewansh.mca21.du@gmail.com>
2025-04-11 10:55:56 +0000

    import react-router-dom in main file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header/Header";
+import {BrowserRouter} from "react-router-dom"
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
