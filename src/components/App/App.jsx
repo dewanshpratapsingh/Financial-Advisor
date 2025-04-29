1c2ac9090f22628e07568a4f0416fa73cbbcc84d
dewanshpratapsingh <dewansh.mca21.du@gmail.com>
2025-04-29 10:34:29 +0000

    Updated app to stop reload on page change

---
+++ src/components/App/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+
+const Home = () => <div>Home Page</div>;
+const About = () => <div>About Page</div>;
+const Contact = () => <div>Contact Page</div>;
+
+const App= ()=>{
+    return(
+        <>
+            < Header title="Financial Advisor"/>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/about" element={<About />}/>
+                <Route path="/contact" element={<Contact />}/>
+            </Routes>
+        </>
+    )
+}
+
+export default App;
